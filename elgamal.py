8ec8025048440b82e85cde756f65df2328a2f3ef
aminiaa181611 <84322351+aminiaa181611@users.noreply.github.com>
2021-05-31 17:43:19 -0700

    Update elgamal.py

--- elgamal.py
+++ elgamal.py
@@ -1,14 +1,13 @@
 import random
-from math import pow
+import math
 from params import p
 from params import g
 
 def keygen():
     q = int( (p - 1)/2 )
-    print("p:", p)
-    print("q:", q)
-    a = random.sample(range(1, q + 1), 1)[0]
+    a = random.SystemRandom().randint(1, q)
     sk = a
+    # h = g**a % p
     h = pow(g, a, p)
     pk =  h
     return pk,sk
@@ -16,20 +15,23 @@ def keygen():
 def encrypt(pk,m):
     h = pk
     q = int( (p - 1)/2 )
-    r = random.sample(range(1, q + 1), 1)[0]
+    r = random.SystemRandom().randint(1, q)
+    # c1 = g**r % p
     c1 = pow(g, r, p)
-    c2 = (h**r * m) % p
+    # c2 = (h**r * m) % p
+    c2 = m * pow(h,r,p) % p
     return [c1,c2]
 
 def decrypt(sk,c):
     a = sk
-    m = (c[1]/(c[0]**a)) % p
+    m = c[0]**(p-1-a)*c[1] % p
+    m = c[1] * pow(c[0], p-1-a,p) % p
     return m
 
-
-pk, sk = keygen()
-c = encrypt(pk, 100)
-m = decrypt(sk, c)
-print(m)
-
+# pk, sk = keygen()
+# m = random.SystemRandom().randint(1, p)
+# print("m:", m)
+# c = encrypt(pk, m)
+# m = decrypt(sk, c)
+# print(m, '==', decrypt(sk, c))
 
