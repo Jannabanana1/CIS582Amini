93d633c119e2c6c88fd7c6f4ba22de26740abf0f
aminiaa181611 <84322351+aminiaa181611@users.noreply.github.com>
2021-06-11 12:30:43 -0700

    Create num_BTC.py

---
+++ num_BTC.py
@@ -0,0 +1,13 @@
+import math
+
+def num_BTC(b):
+    ntokens = 0
+    reward = 50
+    blocks = 1
+    while (blocks <= b):
+        if (blocks % 210_000==0):
+            reward = reward / 2
+        ntokens = ntokens + reward
+        blocks = blocks + 1
+
+    return float(ntokens)
