3cdc1619f2cadcef195b894f64062630ca009189
aminiaa181611 <84322351+aminiaa181611@users.noreply.github.com>
2021-06-11 19:53:11 -0700

    Update num_BTC.py

--- num_BTC.py
+++ num_BTC.py
@@ -1,13 +1,26 @@
 import math
 
 def num_BTC(b):
-    ntokens = 0
+    ntokens = 50
     reward = 50
     blocks = 1
-    while (blocks <= b):
+    while (blocks < b):
         if (blocks % 210_000==0):
             reward = reward / 2
         ntokens = ntokens + reward
         blocks = blocks + 1
 
     return float(ntokens)
+
+b = 648559; tb = 18490993.75
+# b = 890884
+b = 813574; tb = 19522337.5
+# b = 392344; tb = 15058600.00
+# b = 462283; tb = 16278537.5
+r = num_BTC(b)
+print("num_BTC(",b,") = ",r, ": ", r-tb)
+
+# 25.0
+# 12.5
+# 6.25
+# 3.15
