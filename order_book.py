af2fe37ded6c1cd891a28ea00fff54991e90d2b6
aminiaa181611 <84322351+aminiaa181611@users.noreply.github.com>
2021-07-20 18:09:16 -0500

    Update order_book.py

--- order_book.py
+++ order_book.py
@@ -12,14 +12,17 @@ def process_order(order_dict):
     order = Order( sender_pk=order_dict['sender_pk'],receiver_pk=order_dict['receiver_pk'], buy_currency=order_dict['buy_currency'], sell_currency=order_dict['sell_currency'], buy_amount=order_dict['buy_amount'], sell_amount=order_dict['sell_amount'], creator_id=order_dict.get('creator_id', None))
     session.add(order)
     session.commit()
-    orders = session.query(Order).filter(Order.filled==None).all()
+    orders = session.query(Order).all()
+
     for existing_order in orders:
         if match_found(order, existing_order):
             order.filled = datetime.now()
             existing_order.filled = datetime.now()
             order.counterparty_id = existing_order.id
             existing_order.counterparty_id = order.id   
+            session.commit()
             if order.buy_amount > existing_order.sell_amount:
+                print("partially filled")
                 new_order_dict = {}
                 new_order_dict['sender_pk'] = order.sender_pk
                 new_order_dict['receiver_pk'] = order.receiver_pk
@@ -30,7 +33,7 @@ def process_order(order_dict):
                 new_order_dict['sell_amount'] = 1.01*(new_buy_amount * order.buy_amount / order.sell_amount)
                 new_order_dict['creator_id'] = order.id
                 process_order(new_order_dict)
-
+                
 
 def match_found(order, existing_order):
     if existing_order.filled==None:
@@ -39,4 +42,3 @@ def match_found(order, existing_order):
                 if existing_order.sell_amount / existing_order.buy_amount >= order.buy_amount / order.sell_amount:
                     return True
     return False
-
