10a66563540ce7aa39adf1d3506491aa07c9aadf
aminiaa181611 <84322351+aminiaa181611@users.noreply.github.com>
2021-07-21 12:27:34 -0500

    Update order_book.py

--- order_book.py
+++ order_book.py
@@ -19,17 +19,18 @@ def process_order(order_dict):
             existing_order.filled = datetime.now()
             order.counterparty_id = existing_order.id
             existing_order.counterparty_id = order.id   
-            if order.buy_amount > existing_order.sell_amount:
-                new_order_dict = {}
-                new_order_dict['sender_pk'] = order.sender_pk
-                new_order_dict['receiver_pk'] = order.receiver_pk
-                new_order_dict['buy_currency'] = order.buy_currency
-                new_order_dict['sell_currency'] = order.sell_currency
-                new_buy_amount = order.buy_amount - existing_order.sell_amount
-                new_order_dict['buy_amount'] = new_buy_amount
-                new_order_dict['sell_amount'] = 1.01*(new_buy_amount * order.buy_amount / order.sell_amount)
-                new_order_dict['creator_id'] = order.id
-                process_order(new_order_dict)
+            session.commit()
+            # if order.buy_amount > existing_order.sell_amount:
+            #     new_order_dict = {}
+            #     new_order_dict['sender_pk'] = order.sender_pk
+            #     new_order_dict['receiver_pk'] = order.receiver_pk
+            #     new_order_dict['buy_currency'] = order.buy_currency
+            #     new_order_dict['sell_currency'] = order.sell_currency
+            #     new_buy_amount = order.buy_amount - existing_order.sell_amount
+            #     new_order_dict['buy_amount'] = new_buy_amount
+            #     new_order_dict['sell_amount'] = 1.01*(new_buy_amount * order.buy_amount / order.sell_amount)
+            #     new_order_dict['creator_id'] = order.id
+            #     process_order(new_order_dict)
 
 
 def match_found(order, existing_order):
