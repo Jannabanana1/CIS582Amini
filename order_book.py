64cdd0c6efd08d7d1c541a48c8f037ad36b91eb2
aminiaa181611 <84322351+aminiaa181611@users.noreply.github.com>
2021-07-21 14:27:51 -0500

    Update order_book.py

--- order_book.py
+++ order_book.py
@@ -1,4 +1,3 @@
-from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime
 
@@ -11,13 +10,13 @@ session = DBSession()
 
 
 def process_order(order_dict):
-    order = Order(sender_pk=order_dict['sender_pk'], receiver_pk=order_dict['receiver_pk'],
-                  buy_currency=order_dict['buy_currency'], sell_currency=order_dict['sell_currency'],
-                  buy_amount=order_dict['buy_amount'], sell_amount=order_dict['sell_amount'],
-                  creator_id=order_dict.get('creator_id', None))
+    order = Order( sender_pk=order_dict['sender_pk'],receiver_pk=order_dict['receiver_pk'], buy_currency=order_dict['buy_currency'], sell_currency=order_dict['sell_currency'], buy_amount=order_dict['buy_amount'], sell_amount=order_dict['sell_amount'] )
+
     session.add(order)
     session.commit()
-    orders = session.query(Order).filter(Order.filled == "").all()
+
+    orders = session.query(Order).filter(Order.filled == None).all()
+
     for existing_order in orders:
         if match_found(order, existing_order):
             order.filled = datetime.now()
@@ -26,37 +25,40 @@ def process_order(order_dict):
             existing_order.counterparty_id = order.id
 
             if order.buy_amount > existing_order.sell_amount:
-                new_order = {}
-                new_order['creator_id'] = order.id
-                new_order['sender_pk'] = order.sender_pk
-                new_order['receiver_pk'] = order.receiver_pk
-                new_order['buy_currency'] = order.buy_currency
-                new_order['sell_currency'] = order.sell_currency
+                new_order_dict = {}
+                new_order_dict['creator_id'] = order.id
+                new_order_dict['sender_pk'] = order.sender_pk
+                new_order_dict['receiver_pk'] = order.receiver_pk
+                new_order_dict['buy_currency'] = order.buy_currency
+                new_order_dict['sell_currency'] = order.sell_currency
                 new_buy_amount = order.buy_amount - existing_order.sell_amount
-                new_order['buy_amount'] =  new_buy_amount
-                new_order['sell_amount'] = (new_buy_amount * order.sell_amount / order.buy_amount)
-                new_order = Order(creator_id=new_order['creator_id'], sender_pk=new_order['sender_pk'],
-                                  receiver_pk=new_order['receiver_pk'], buy_currency=new_order['buy_currency'],
-                                  sell_currency=new_order['sell_currency'], buy_amount=new_order['buy_amount'],
-                                  sell_amount=new_order['sell_amount'])
-                process_order(new_order)
+                new_order_dict['buy_amount'] = new_buy_amount
+                new_order_dict['sell_amount'] = new_buy_amount * order.buy_amount / order.sell_amount
+                new_order = Order(creator_id=new_order_dict['creator_id'], sender_pk=new_order_dict['sender_pk'],
+                                  receiver_pk=new_order_dict['receiver_pk'],
+                                  buy_currency=new_order_dict['buy_currency'],
+                                  sell_currency=new_order_dict['sell_currency'],
+                                  buy_amount=new_order_dict['buy_amount'], sell_amount=new_order_dict['sell_amount'])
+                session.add(new_order)
+                session.commit()
 
             elif order.buy_amount < existing_order.sell_amount:
-                new_order = {}
-                new_order['creator_id'] = existing_order.id
-                new_order['sender_pk'] = existing_order.sender_pk
-                new_order['receiver_pk'] = existing_order.receiver_pk
-                new_order['buy_currency'] = existing_order.buy_currency
-                new_order['sell_currency'] = existing_order.sell_currency
+                new_order_dict = {}
+                new_order_dict['creator_id'] = existing_order.id
+                new_order_dict['sender_pk'] = existing_order.sender_pk
+                new_order_dict['receiver_pk'] = existing_order.receiver_pk
+                new_order_dict['buy_currency'] = existing_order.buy_currency
+                new_order_dict['sell_currency'] = existing_order.sell_currency
                 new_sell_amount = existing_order.sell_amount - order.buy_amount
-                new_order['buy_amount'] =  new_sell_amount
-                new_order['sell_amount'] =(new_sell_amount * existing_order.buy_amount / existing_order.sell_amount)
-                new_order = Order(creator_id=new_order['creator_id'], sender_pk=new_order['sender_pk'],
-                                  receiver_pk=new_order['receiver_pk'], buy_currency=new_order['buy_currency'],
-                                  sell_currency=new_order['sell_currency'], buy_amount=new_order['buy_amount'],
-                                  sell_amount=new_order['sell_amount'])
+                new_order_dict['sell_amount'] = new_sell_amount
+                new_order_dict['buy_amount'] = new_sell_amount * existing_order.buy_amount / existing_order.sell_amount
+                new_order = Order(creator_id=new_order_dict['creator_id'], sender_pk=new_order_dict['sender_pk'],
+                                  receiver_pk=new_order_dict['receiver_pk'], buy_currency=new_order_dict['buy_currency'],
+                                  sell_currency=new_order_dict['sell_currency'], buy_amount=new_order_dict['buy_amount'],
+                                  sell_amount=new_order_dict['sell_amount'])
+                session.add(new_order)
+                session.commit()
 
-                process_order(new_order)
 
 def match_found(order, existing_order):
     if existing_order.filled == None:
