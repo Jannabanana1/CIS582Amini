b5f07f26bbfea1bf79fd1d0a3b9c1b1bb5043ed3
aminiaa181611 <84322351+aminiaa181611@users.noreply.github.com>
2021-07-21 15:24:16 -0500

    Update order_book.py

--- order_book.py
+++ order_book.py
@@ -8,7 +8,6 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-
 def process_order(order_dict):
     sender_pk = order_dict['sender_pk']
     receiver_pk = order_dict['receiver_pk']
@@ -42,7 +41,8 @@ def process_order(order_dict):
     orders = session.query(Order).filter(Order.filled==None).all()
     
     for existing_order in orders:
-      if find_match(order, existing_order):
+      is_match_found = find_match(order, existing_order)
+      if is_match_found:
         order.filled = datetime.now()
         existing_order.filled = datetime.now()
         order.counterparty_id = existing_order.id
@@ -58,10 +58,8 @@ def process_order(order_dict):
                 child['buy_amount'] = new_buy_amount
                 child['sell_amount'] = 1.1*(new_buy_amount * order.sell_amount/order.buy_amount )
                 child['creator_id'] = order.id
-                # child = Order(creator_id=child['creator_id'], sender_pk=child['sender_pk'],receiver_pk=child['receiver_pk'], buy_currency=child['buy_currency'], sell_currency=child['sell_currency'], buy_amount=child['buy_amount'], sell_amount=child['sell_amount'] )
                 process_order(child)
-                # session.add(child)
-                # session.commit()
+
         if existing_order.sell_amount > order.buy_amount:
                 child = {}
                 child['sender_pk'] = existing_order.sender_pk
@@ -72,14 +70,12 @@ def process_order(order_dict):
                 child['sell_amount'] = new_sell_amount
                 child['buy_amount'] = 0.9*(new_sell_amount * existing_order.buy_amount/existing_order.sell_amount )
                 child['creator_id'] = existing_order.id
-                # child = Order(creator_id=child['creator_id'], sender_pk=child['sender_pk'],receiver_pk=child['receiver_pk'], buy_currency=child['buy_currency'], sell_currency=child['sell_currency'], buy_amount=child['buy_amount'], sell_amount=child['sell_amount'] )
                 process_order(child)
-                # session.add(child)
-                # session.commit()
-
-
 
 def find_match(order, existing_order):
-        if order.filled == None and order.sell_currency==existing_order.buy_currency and order.buy_currency== existing_order.sell_currency and existing_order.sell_amount / existing_order.buy_amount >= order.buy_amount / order.sell_amount:
-          return True 
-        return False
+    if existing_order.filled==None:
+        if existing_order.buy_currency==order.sell_currency:
+            if existing_order.sell_currency==order.buy_currency:
+                if existing_order.sell_amount / existing_order.buy_amount >= order.buy_amount / order.sell_amount:
+                    return True
+    return False
